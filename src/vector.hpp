12996333ce23874f1f8d172d29c53ad284daa08e
Wiktor Bajor <wiktorbajor1@gmail.com>
2020-09-15 19:05:54 +0200

    Adding first ctor.

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,14 +1,29 @@
 #pragma once
 
-#include<iterator>
+#include <cstddef>
+
+#include <algorithm>
+#include <iterator>
+
+template <typename T, typename Allocator = std::allocator<T>>
+class Vector {
+    using value_type = T;
+
+  public:
+    explicit Vector(std::size_t size){
+        first_ = allocator_.allocate(size);
+        realEnd_ = end_ = first_ + size;
+        auto first = first_;
+        auto end = end_;
+        for(; first != end; ++first)
+            new(first) T();
+    }
+    std::size_t size()const{return end_-first_;}
 
 
-class vector
-{
-private:
-    /* data */
-public:
-    vector(/* args */);
-    ~vector();
+  private:
+    Allocator allocator_;
+    T *first_ = {};
+    T *end_ = {};
+    T *realEnd_ = {};
 };
-
