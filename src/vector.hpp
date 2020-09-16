de4cf3803fadd95bcf131e099a1a100e25b4a28d
Wiktor Bajor <wiktorbajor1@gmail.com>
2020-09-16 11:56:37 +0200

    Added test to ctor with default value, minimal begin(), end() method for tests.

--- src/vector.hpp
+++ src/vector.hpp
@@ -10,19 +10,20 @@ class Vector {
     using value_type = T;
 
   public:
-    explicit Vector(std::size_t size){
+    explicit Vector(std::size_t size, const T& value = T()){
         first_ = allocator_.allocate(size);
         realEnd_ = end_ = first_ + size;
         auto first = first_;
         auto end = end_;
         for(; first != end; ++first)
-            new(first) T();
+            new(first)T(value);
     }
     std::size_t size()const{return end_-first_;}
-
+    auto begin(){return first_;}
+    auto end(){return end_;}
 
   private:
-    Allocator allocator_;
+    Allocator allocator_ = {};
     T *first_ = {};
     T *end_ = {};
     T *realEnd_ = {};
--- tests/vector.ut.cpp
+++ tests/vector.ut.cpp
@@ -1,9 +1,24 @@
-#include "catch.hpp"
+
+#include <random>
+#include <vector>
+
 #include "../src/vector.hpp"
+#include "catch.hpp"
 
+TEST_CASE("Construct Vector with Vector<int>(random_number) shuld give vectro.size() = random_number ") {
+    std::random_device rd;
+    std::mt19937 e{rd()};
 
-TEST_CASE("Construct Vector with Vector<int>(2) shuld give vectro.size() = 2 "){
-    Vector<int> vector(2);
-    
-    REQUIRE(vector.size() == 2);
+    std::uniform_int_distribution<int> dist{
+        1, 10000000};
+    std::size_t random_number = dist(e);
+
+    Vector<int> vector(random_number);
+    REQUIRE(vector.size() == random_number);
+}
+
+TEST_CASE("Constructed Vector<int>(5) should contain only 0 values{0,0,0,0,0}"){
+    int expected[5]  = {0,0,0,0,0};
+    Vector<int> vector(5);
+    REQUIRE(std::equal(vector.begin(),vector.end(),expected,expected+5));
 }
